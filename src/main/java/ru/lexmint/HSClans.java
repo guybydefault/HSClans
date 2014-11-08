60786119c73c7940e532d60bd3f2ffd71894074a
Lexmint <lexmint@rambler.ru>
2014-11-08 21:43:29 +0300

    Тело плагина

---
+++ src/main/java/ru/lexmint/HSClans.java
@@ -0,0 +1,27 @@
+package ru.lexmint;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import ru.lexmint.cmd.CommandManager;
+import ru.lexmint.domain.Clan;
+
+/**
+ * Core of the plugin.
+ */
+public class HSClans extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("***** ENABLING " + getDescription().getName() + " " + getDescription().getVersion() + " *****");
+        long startingTime = System.currentTimeMillis();
+
+        for(String command : getDescription().getCommands().keySet()) {
+            getCommand(command).setExecutor(new CommandManager());
+        }
+
+        getLogger().info(getDescription().getName() + " " + getDescription().getVersion() + " has been successfully enabled! (" + (System.currentTimeMillis() - startingTime) + "ms)");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info(getDescription().getName() + " " + getDescription().getVersion() + " has been disabled!");
+    }
+}
